77dde5941dc5767ed0ce9841847a68361972038e
mahantayyahiremath <mahantayyahiremath@mahantayyas-mbp>
2018-12-19 17:13:27 +0000

    CORS enabled

--- src/main/java/com/nttdata/app/repository/ValueCreationIdeaRepository.java
+++ src/main/java/com/nttdata/app/repository/ValueCreationIdeaRepository.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.nttdata.app.model.Idea;
 
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "idea", path = "idea")
 public interface ValueCreationIdeaRepository extends PagingAndSortingRepository<Idea, Long> {
 	List<Idea> findByUserId(@Param("userId") String userId);
